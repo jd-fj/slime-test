0daa0733548842211dca9e08c1b58ca5b6266f74
taylulz <taylulzcode@gmail.com>
2020-11-25 11:13:09 -0800

    working mouseenter mouseleave features

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
   var x = document.getElementById("myAudio");
+
+  // Play is triggered when mouse is over image
+  $("#picture").mouseenter(function() { 
+    x.play(); 
+  });
   
+  // Pause is triggered when mouse leaves image
+  $("#picture").mouseleave(function() { 
+    x.pause(); 
+  });
+
+
+  // Play and Pause can be triggered with Play/Pause buttons
   $("#play-btn").click(function() { 
     x.play(); 
   });
-
   $("#pause-btn").click(function() { 
     x.pause(); 
   });
