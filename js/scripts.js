2ab0e5043ff22b276ce42552819fd201595c5701
taylulz <taylulzcode@gmail.com>
2020-11-25 10:59:48 -0800

    working play button

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  var x = $("#myAudio"); 
-
-  $("#play-btn").click(function playAudio() { 
+  var x = document.getElementById("myAudio");
+  
+  $("#play-btn").click(function() { 
     x.play(); 
   });
 
-  $("#pause-btn").click(function pauseAudio() { 
+  $("#pause-btn").click(function() { 
     x.pause(); 
   });
 });
