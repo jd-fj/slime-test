82a5f58c162f6188fc214ba8c19ae00396e460c8
taylulz <taylulzcode@gmail.com>
2020-11-28 13:41:12 -0800

    add base sound loop, click for randomized sound to play on top

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,34 @@
 $(document).ready(function() {
-  var x = document.getElementById("myAudio"); // you need to use getElementById as using jQuery here was not working. .loop at the end of this should make the audio loop, but not working yet
+  const x = document.getElementById("myAudio"); // you need to use getElementById as using jQuery here was not working. .loop at the end of this should make the audio loop, but not working yet
+  const base = document.getElementById("base")
+  const sound1 = document.getElementById("sound1");
+  const sound2 = document.getElementById("sound2");
+  const sound3 = document.getElementById("sound3");
+  const sound4 = document.getElementById("sound4");
+  const sound5 = document.getElementById("sound5");
+  const array = [sound1, sound2, sound3, sound4, sound5];
+  
 
   // Play is triggered when mouse is over image
   $("#picture").mouseenter(function() { 
-    x.play(); 
+    base.play(); 
+    
+  });
+
+  $("#picture").click(function() { 
+    let rando = Math.floor(Math.random() * Math.floor(4));
+    array[rando].play();
   });
   
   // Pause is triggered when mouse leaves image
   $("#picture").mouseleave(function() { 
-    x.pause(); 
+
   });
 
 
   // Play and Pause can be triggered with Play/Pause buttons
   $("#play-btn").click(function() { 
-    x.play(); 
+    sound1.loop.play(); 
   });
   $("#pause-btn").click(function() { 
     x.pause(); 
